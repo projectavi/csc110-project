b442ed4ebeba63d232cae00971f479ed86d14b5e
Rudy <rudraksh.monga@mail.utoronto.ca>
2021-12-13 23:19:01 -0500

    added instance attributes to cleaning_dataset

--- final/cleaned-data/cleaning_dataset.py
+++ final/cleaned-data/cleaning_dataset.py
@@ -6,12 +6,16 @@ Created by Mishaal Kandapath, Taylor Whatley, Aviraj Newatia, and Rudraksh Monga
 # import numpy as np
 
 
-def remove_columns(dfs: list, cols: list) -> None:
+def remove_columns(dfs: list, cols: list[str]) -> None:
     """
     Remove the specified columns from the given dataframes.
 
     Precondition:
       - all(df1.columns == df2.columns for df1 in dfs for df2 in dfs)
+
+    Instance Attributes:
+      - dfs: a list containing all the dataframes that need to have columns removed.
+      - cols: a list of strings containing the names of the columns that need to be removed.
     """
     for df in dfs:
         df.drop(columns=cols, inplace=True)
@@ -20,6 +24,9 @@ def remove_columns(dfs: list, cols: list) -> None:
 def add_id(dfs: list) -> None:
     """
     Add an id column to the input dataframes.
+
+    Instance Attributes:
+      - dfs: a list containing all the dataframes that need to have an 'id' column inserted.
     """
     for df in dfs:
         df.insert(0, 'id', df.index)
@@ -29,6 +36,10 @@ def add_id(dfs: list) -> None:
 def drop_null_rows(dfs: list) -> None:
     """
     Drop all rows that are missing any values.
+
+    Instance Attributes:
+      - dfs: a list containing all the dataframes that need to have an rows with empty values
+        removed.
     """
     for df in dfs:
         df.dropna(inplace=True)
@@ -36,29 +47,11 @@ def drop_null_rows(dfs: list) -> None:
 
 def clean_dataset(dfs: list, cols: list) -> None:
     """
-    Clean the input datasets using previously defined functions.
+    Clean the input datasets using previously defined functions.\
+
+    Instance Attributes:
+      - dfs: a list containing all the dataframes that need to be cleaned.
     """
     remove_columns(dfs, cols)
     add_id(dfs)
     drop_null_rows(dfs)
-
-
-if __name__ == '__main__':
-    import python_ta.contracts
-
-    python_ta.contracts.check_all_contracts()
-
-    import doctest
-
-    doctest.testmod()
-
-    import python_ta
-
-    python_ta.check_all(config={
-        'extra-imports': ["doctest", "python_ta", "datetime", "praw"],
-        # the names (strs) of imported modules
-        'allowed-io': ["get_comments_subreddits"],
-        # the names (strs) of functions that call print/open/input
-        'max-line-length': 100,
-        'disable': ['R1705', 'C0200']
-    })
