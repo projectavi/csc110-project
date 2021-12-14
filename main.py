2e4964ec4e2aa26c729770209125f334989c90f0
desgroup <32211852+1whatleytay@users.noreply.github.com>
2021-12-14 05:38:11 -0500

    better pi

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ Copyright Mishaal Kandapath, Taylor Whatley, Aviraj Newatia, and Rudraksh Monga.
 import datetime
 import pandas
 
-from stats import StatisticsCommentInfo, graph_raw, process_average_comments
+from stats import StatisticsCommentInfo, graph_raw, process_average_comments, filter_comments
 from sentiment_analysis_naive_bayes import SentimentAnalyzer
 from simulation import OpinionSimulationManager
 
 
 def load_and_graph(analyzer: SentimentAnalyzer,
-                   path: str, title: str, keys: tuple[str, str]) -> None:
+                   path: str, title: str, keys: tuple[str, str],
+                   date_range: tuple[datetime.datetime, datetime.datetime]) -> None:
     """
     Loads the CSV file at `path`, analyzes comment text with `analyzer`
     and graphs the results in a graph named `title`.
+
+    Only comments between `date_range[0]` and `date_range[1]` are graphed.
 
     The CSV file at `path` is assumed to have two columns
     named after the values of `keys[0]` and `keys[1]` respectively.
@@ -37,6 +40,8 @@ def load_and_graph(analyzer: SentimentAnalyzer,
 
         for i in range(len(data))
     ]
+
+    comments = filter_comments(comments, date_range[0], date_range[1])
 
     data = process_average_comments(comments)
 
@@ -63,7 +68,7 @@ if __name__ == '__main__':
 
     # Parameters
     analyzer_source_model = 'datasets/exports.json'
-    simulation_seed_comment = 'I really like python.'
+    simulation_seed_comment = 'I really like fortnite.'
 
     # Some of the CSV files include sentiment data 3 days before the sentiment model was pushed.
     # I'm just going to assume the sentiment data in the pandas model is faulty.
@@ -75,12 +80,14 @@ if __name__ == '__main__':
     load_and_graph(sentiment_analyzer,
                    'datasets/pre-covid-us.csv',
                    'Comment Sentiment Over Time (Pre Covid US)',
-                   ('timestamp', 'body'))
+                   ('timestamp', 'body'),
+                   (datetime.datetime(2019, 4, 1), datetime.datetime(2021, 2, 28)))
 
     load_and_graph(sentiment_analyzer,
                    'datasets/post-covid-us.csv',
                    'Comment Sentiment Over Time (Post Covid US)',
-                   ('created_utc', 'body'))
+                   ('created_utc', 'body'),
+                   (datetime.datetime(2021, 2, 28), datetime.datetime.now()))
 
     simulation = OpinionSimulationManager()
     simulation.add_sim_instance(100)
