5365a02bbbc5e40297158496a807779f2dd43815
ecomparer <mishaalhere@gmail.com>
2021-12-13 23:06:42 -0500

    pytohn ta fixes

--- train.py
+++ train.py
@@ -2,10 +2,14 @@
 Running a Naive Bayes Sentiment Analysis model.
 Created by Mishaal Kandapath, Taylor Whatley, Aviraj Newatia, and Rudraksh Monga.
 """
-from sentiment_analysis_naive_bayes import SentimentAnalyzer
+
 import csv
-import random
 import sys
+import random
+
+from sentiment_analysis_naive_bayes import SentimentAnalyzer
+
+
 def obtain_training_data(filename: str) -> list[tuple[str, str]]:
     """
     Obtain the training data from the given file.
@@ -19,7 +23,9 @@ def obtain_training_data(filename: str) -> list[tuple[str, str]]:
             data.append((tweet, label))
     return data
 
-def split_training_test(data: list[tuple[str, str]]) -> tuple[list[tuple[str, str]], list[tuple[str, str]]]:
+
+def split_training_test(data: list[tuple[str, str]])\
+        -> tuple[list[tuple[str, str]], list[tuple[str, str]]]:
     """
     Split the training data into training and test data.
     """
@@ -41,26 +47,46 @@ def evaluate_model(test_data: list[tuple[str, str]], analyzer: SentimentAnalyzer
         if analyzer.classify(tweet)[0] == label:
             correct += 1
         count += 1
-        sys.stdout.write('\r'+"progress: " + "{:.4f}".format(round(count / len(test_data) * 100, 4)) + "%"+ " "+ "accuracy: " + "{:.4f}".format(round(100  * correct / count, 4)) + "%")
+        sys.stdout.write('\rprogress: ' + "{:.4f}".format(round(count / len(test_data) * 100, 4))
+                         + "% accuracy: "
+                         + "{:.4f}".format(round(100 * correct / count, 4)) + "%")
         sys.stdout.flush()
     return correct / len(test_data)
 
+
 if __name__ == '__main__':
+    import python_ta.contracts
+
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+
+    doctest.testmod()
+
+    import python_ta
+
+    python_ta.check_all(config={
+        'extra-imports': [
+            'csv', 'random', 'sys', 'sentiment_analysis_naive_bayes'
+        ],
+        'allowed-io': ['obtain_training_data'],
+        'max-line-length': 100,
+        'disable': ['R1705', 'C0200']
+    })
+
     print("initializing...")
-    analyzer = SentimentAnalyzer()
+    sentiment_analyzer = SentimentAnalyzer()
     print("loading training data...")
-    training_data = obtain_training_data('training.csv')
-    training_data, test_data = split_training_test(training_data)
+    train_data = obtain_training_data('training.csv')
+    train_data, test = split_training_test(train_data)
     print("finished loading training data.")
     print("training model...")
-    analyzer.train(training_data)
+    sentiment_analyzer.train(train_data)
     print("finished training model.")
-    analyzer.export_trained_data('exports.json')
-    print("exported data", analyzer.priors)
-    print(analyzer.classify("I hate this movie"))
+    sentiment_analyzer.export_trained_data('exports.json')
+    print("exported data", sentiment_analyzer.priors)
+    print(sentiment_analyzer.classify("I hate this movie"))
     print("evaluating model...")
-    evluation_accuracy = evaluate_model(test_data, analyzer)
-    print("", end= "\n")
+    evluation_accuracy = evaluate_model(test, sentiment_analyzer)
+    print("", end="\n")
     print("evaluation accuracy:", evluation_accuracy)
-
-    
